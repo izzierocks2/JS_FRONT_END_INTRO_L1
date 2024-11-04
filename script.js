c1b77d166693cc98e94c713777c6c46612548aa5
Grace <gracek2705@tuckahoeschools.org>
2024-11-04 13:31:03 +0000

    init commit

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function changeTextColor(){
     });
 }
 changeTextColor();
+
+//---------------------------------------------------------//
+const pictureElement = document.getElementById("toggle-image");
+function changePicture(){
+    const images = [
+        "https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_giraffe_1_0.jpg",
+        "https://i.natgeofe.com/n/5f35194b-af37-4f45-a14d-60925b280986/NationalGeographic_2731043_square.jpg",
+        "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+        "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg",
+        "https://static01.nyt.com/images/2024/05/05/multimedia/05DOG-AGGRESSION-qhgl/05DOG-AGGRESSION-qhgl-mediumSquareAt3X.jpg",
+        "https://hips.hearstapps.com/hmg-prod/images/golden-retriever-dog-royalty-free-image-505534037-1565105327.jpg?crop=0.760xw:1.00xh;0.204xw,0&resize=980:*"
+    ];
+    
+    let currentIndex = 0;
+    
+    pictureElement.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        pictureElement.src = images[currentIndex];
+    });
+}
+
+changePicture();
